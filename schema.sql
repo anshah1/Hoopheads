1d267d11c9c22d65c5ac21444e840ff010223d12
anshah1 <anshshah2025@gmail.com>
2025-06-03 16:42:13 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,20 @@
-CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE stats (
-    personUsername INTEGER,
-    ones INTEGER DEFAULT 0,
-    two INTEGER DEFAULT 0,
-    three INTEGER DEFAULT 0,
-    four INTEGER DEFAULT 0,
-    five INTEGER DEFAULT 0,
-    six INTEGER DEFAULT 0,
-    sevens INTEGER DEFAULT 0,
-    eights INTEGER DEFAULT 0,
-    ails INTEGER DEFAULT 0,
-    FOREIGN KEY (personUsername) REFERENCES users(ID)
+CREATE TABLE users (
+ id SERIAL PRIMARY KEY,
+ username TEXT NOT NULL UNIQUE,
+ hash TEXT NOT NULL,
+ streak NUMERIC NOT NULL,
+ winCount NUMERIC NOT NULL,
+ gamesPlayed NUMERIC NOT NULL
 );
+CREATE TABLE stats (
+    personUsername TEXT NOT NULL,    
+    ones INTEGER DEFAULT 0,    
+    two INTEGER DEFAULT 0,    
+    three INTEGER DEFAULT 0,    
+    four INTEGER DEFAULT 0,    
+    five INTEGER DEFAULT 0,    
+    six INTEGER DEFAULT 0,    
+    sevens INTEGER DEFAULT 0,    
+    eights INTEGER DEFAULT 0,    
+    fails INTEGER DEFAULT 0,    
+    FOREIGN KEY (personUsername) REFERENCES users(username));
